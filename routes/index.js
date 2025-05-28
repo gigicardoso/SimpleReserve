b586d0847debb4cfcfc9cbea6f412f2534962575
giovanna <giovannacardoso.ifc@gmail.com>
2025-05-28 12:37:24 -0300

    Arrumado rota de login

--- routes/index.js
+++ routes/index.js
@@ -12,5 +12,7 @@ router.get('/', (req, res) => {
 router.get('/cadastrosala', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'cadastroSala.html'));
 });
-
+router.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'login.html'));
+});
 module.exports = router;
