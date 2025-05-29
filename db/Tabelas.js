8c10947313a6d5c360d06f05f749dcc6e95e2e70
giovanna <giovannacardoso.ifc@gmail.com>
2025-05-29 14:36:19 -0300

    Arrumado cadastro de sala e organizado css

--- db/Tabelas.js
+++ db/Tabelas.js
@@ -1,16 +1,16 @@
-const db = require('./db');
+const db = require("./db");
 
 //aqui é onde vamos colocar as tabelas do banco de dados
 //'postagens' é o nome da tabela
 
-const Posts = db.sequelize.define('postagens', {
-    titulo: {
-        type: db.Sequelize.STRING
-    },
-    conteudo: {
-        type: db.Sequelize.TEXT
-    }
-}); 
+const Posts = db.sequelize.define("postagens", {
+  titulo: {
+    type: db.Sequelize.STRING,
+  },
+  conteudo: {
+    type: db.Sequelize.TEXT,
+  },
+});
 
 module.exports = Posts;
 
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const path = require('path');
-const db = require('../db/db');
+const path = require("path");
+const db = require("../db/db");
 
 /* Rota para Home */
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'login.html'));
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "login.html"));
 });
 
 /* Rota para cadastro de sala */
-router.get('/cadastrosala', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'cadastroSala.html'));
+router.get("/cadastrosala", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "cadastroSala.html"));
 });
-router.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'login.html'));
+router.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "login.html"));
 });
 module.exports = router;
