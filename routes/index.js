d2b15af13613b3139ee1837bb14ffbe1bec9c045
giovanna <giovannacardoso.ifc@gmail.com>
2025-06-01 11:49:21 -0300

    sidebar adicionada ao layout

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,33 @@ const db = require("../db/db");
 
 /* Rota para Home */
 router.get("/", (req, res) => {
-  res.render(path.join(__dirname, "..", "views", "index.hbs"));
+  res.render(path.join(__dirname, "..", "views", "index.hbs"), {
+    layout: "layout",
+    showSidebar: true,
+  });
 });
 
 /* Rota para cadastro de sala */
 router.get("/cadastrosala", (req, res) => {
-  res.render(path.join(__dirname, "..", "views", "cadastroSala.hbs"));
+  res.render(path.join(__dirname, "..", "views", "cadastroSala.hbs"), {
+    layout: "layout",
+    showSidebar: true,
+  });
 });
+// Para login
 router.get("/login", (req, res) => {
-  res.render(path.join(__dirname, "..", "views", "login.hbs"));
+  res.render(path.join(__dirname, "..", "views", "login.hbs"), {
+    layout: "layout",
+    showSidebar: false,
+  });
 });
+
+// Para cadastro de usuário (futura tela)
+/*router.get("/cadastro", (req, res) => {
+  res.render(path.join(__dirname, "..", "views", "cadastro.hbs"), {
+    layout: "layout",
+    showSidebar: false,
+  });
+});*/
+
 module.exports = router;
