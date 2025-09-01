042339392fdcd31f098b980636fb20c206644a27
giovanna <giovannacardoso.ifc@gmail.com>
2025-09-01 13:29:43 -0300

    Tirado cod da tabela de salas

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,7 +5,7 @@ module.exports = function(req, res, next) {
   
   if (req.session.usuario.id_permissao !== 1) {
     req.session.error = 'Acesso negado. Você não tem permissão para acessar a área administrativa.';
-    return res.redirect('/');
+    return res.redirect('/home');
   }
   
   next();
