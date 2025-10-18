73ef9e556444c84fed6c0af09247e2c636c1d0ca
giovanna <giovannacardoso.ifc@gmail.com>
2025-10-18 14:28:49 -0300

    edição e exclusão de permissão

--- routes/permissoesRouter.js
+++ routes/permissoesRouter.js
@@ -35,4 +35,43 @@ router.post('/', auth, (req, res, next) => {
   next();
 }, permissoesController.criarPermissao);
 
+// Editar permissão (formulário)
+router.get('/editarPermissao/:id_permissao', auth, async (req, res) => {
+  if (!req.session.usuario.isAdm) {
+    return res.render('error', { message: 'Você não tem permissão para editar permissões.', layout: 'layout', showSidebar: true, showLogo: true });
+  }
+  const Permissao = require('../models/permissaoModel');
+  const permissao = await Permissao.findByPk(req.params.id_permissao);
+  if (!permissao) {
+    return res.render('error', { message: 'Permissão não encontrada.', layout: 'layout', showSidebar: true, showLogo: true });
+  }
+  res.render('mais/adicionapermissao', {
+    permissao,
+    layout: 'layout',
+    showSidebar: true,
+    showLogo: true,
+    breadcrumb: [
+      { title: 'Gerenciador ADM', path: '/adm' },
+      { title: 'Permissões', path: '/permissoes' },
+      { title: 'Editar Permissão', path: `/permissoes/editarPermissao/${permissao.id_permissao}` }
+    ]
+  });
+});
+
+// Atualizar permissão (POST)
+router.post('/editarPermissao/:id_permissao', auth, (req, res, next) => {
+  if (!req.session.usuario.isAdm) {
+    return res.render('error', { message: 'Você não tem permissão para editar permissões.', layout: 'layout', showSidebar: true, showLogo: true });
+  }
+  next();
+}, permissoesController.atualizarPermissao);
+
+// Excluir permissão
+router.get('/excluirPermissao/:id', auth, (req, res, next) => {
+  if (!req.session.usuario.isAdm) {
+    return res.render('error', { message: 'Você não tem permissão para excluir permissões.', layout: 'layout', showSidebar: true, showLogo: true });
+  }
+  next();
+}, permissoesController.deletarPermissao);
+
 module.exports = router;
